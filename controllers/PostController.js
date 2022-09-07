ab3e7e2e76068a5df20e95558dc2f1ff413f56ec
Guaderic <VladymyrGolovan@gmail.com>
2022-09-07 13:45:54 +0300

    finish back-end

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,6 +1,20 @@
 import PostModel from "../models/Post.js";
 import e from "express";
 
+export const getLastTags = async (req, res)=>{
+    try {
+        const posts = await PostModel.find().limit(5).exec();
+
+        const tags = posts.map(obj => obj.tags).flat().slice(0, 5)
+        res.json(posts)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: 'Не вдалося знайти статтю'
+        })
+    }
+}
+
 export const create = async (req, res) => {
     try {
         const doc = new PostModel({
